afd9e67ec85c314e45c51298c206e7c2b7e9a474
Camila Marques <milamazim@gmail.com>
2025-08-16 09:31:36 -0300

    Add initial project files including .gitignore, README, and Rectangle implementation

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);             
+        Rectangle ret = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        ret.width = sc.nextDouble();
+        ret.height = sc.nextDouble();
+
+        System.out.printf("AREA = %.2f%n", ret.area());
+        System.out.printf("PERIMETER = %.2f%n", ret.perimeter());
+        System.out.printf("DIAGONAL = %.2f%n", ret.diagonal());
+
+        sc.close();
+
+    }
+}
